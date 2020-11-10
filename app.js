83eaeec100058ee877731c803f7c82c87122201e
BelaM80 <mueller.bela@gmx.de>
2020-11-10 14:26:58 +0100

    Create object for passwords

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
+const inquirer = require("inquirer");
+
 console.log("PW4U");
 
+const secretMasterPassword = "katze";
+const questions = [
+  {
+    type: "input",
+    name: "masterPassword",
+    message: "What is the super secret master password?",
+  },
+];
+const passwordSafe = {
+  wifi: "123",
+  gmx: "qwertz",
+};
+
+async function validateAccess() {
+  const { masterPassword } = await inquirer.prompt(questions);
+
+  if (masterPassword !== secretMasterPassword) {
+    console.error("You are not welcome here! 👿");
+    validateAccess();
+    return;
+  }
+  const password = passwordSafe[passwordName];
+
+  if (password) {
+    console.log(`Password ist ${password}`);
+  } else {
+    console.log("Unknown password");
+  }
+}
 const args = process.argv.slice(2);
 const passwordName = args[0];
 console.log(`You want to know the password of '${passwordName}'`);
 
-if (passwordName === "wifi") {
-  console.log("Password is qwertz");
-} else {
-  console.log("Unknown password");
-}
+validateAccess();
