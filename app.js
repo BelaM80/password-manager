d50226d678d87d54aabfdf09a18898bb80d8e8cc
BelaM80 <mueller.bela@gmx.de>
2020-11-12 11:20:49 +0100

    Create function lib

--- app.js
+++ app.js
@@ -6,45 +6,75 @@
 // const hashDigest = sha256(nonce + message);
 // const hmacDigest = Base64.stringify(hmacSHA512(path + hashDigest, privateKey));
 
-const inquirer = require("inquirer");
-const fs = require("fs");
+// const fs = require("fs");
 
-const questions = [
-  {
-    type: "password",
-    name: "masterPassword",
-    message: "What is the super secret master password?",
-    mask: "*",
-  },
-];
+// const questions = [
+//   {
+//     type: "password",
+//     name: "masterPassword",
+//     message: "What is the super secret master password?",
+//     mask: "*",
+//   },
+// ];
 
-const passwordSafe = JSON.parse(fs.readFileSync("./db.json", "utf-8"));
+// const passwordSafe = JSON.parse(fs.readFileSync("./db.json", "utf-8"));
 
-async function validateAccess() {
-  const args = process.argv.slice(2);
-  const passwordName = args[0];
-  const newPasswordValue = args[1];
-  const { masterPassword } = await inquirer.prompt(questions);
-  const password = passwordSafe[passwordName];
+// async function validateAccess() {
+//   const args = process.argv.slice(2);
+//   const passwordName = args[0];
+//   const newPasswordValue = args[1];
+//   const { masterPassword } = await inquirer.prompt(questions);
+//   const password = passwordSafe[passwordName];
 
-  if ((await masterPassword) !== passwordSafe.master) {
-    console.error("Your password is invalid!");
-    validateAccess();
+//   if ((await masterPassword) !== passwordSafe.master) {
+//     console.error("Your password is invalid!");
+//     validateAccess();
+//     return;
+//   }
+
+//   if (newPasswordValue) {
+//     passwordSafe[passwordName] = newPasswordValue;
+//     fs.writeFileSync("./db.json", JSON.stringify(passwordSafe, null, 2));
+//   } else {
+//     console.log(`You want to know the password of '${passwordName}'`);
+
+//     if (password) {
+//       console.log(`Password ist ${password}`);
+//     } else {
+//       console.log("Unknown password");
+//     }
+//   }
+// }
+
+// validateAccess();
+
+const { readCommandLineArguments } = require("./lib/commadLine");
+const { setPassword, getPassword } = require("./lib/passwords");
+const { askForMasterPassword } = require("./lib/questions");
+const { isMasterPasswordCorrect } = require("./lib/validation");
+
+async function run() {
+  const masterPassword = await askForMasterPassword();
+
+  if (!isMasterPasswordCorrect(masterPassword)) {
+    console.log("That´s not the right password. Try again!");
+    run();
     return;
   }
 
-  if (newPasswordValue) {
-    passwordSafe[passwordName] = newPasswordValue;
-    fs.writeFileSync("./db.json", JSON.stringify(passwordSafe, null, 2));
-  } else {
-    console.log(`You want to know the password of '${passwordName}'`);
+  const [passwordName, newPasswordValue] = readCommandLineArguments();
+  if (!passwordName) {
+    console.log("Missing password name");
+    return process.exit(9);
+  }
 
-    if (password) {
-      console.log(`Password ist ${password}`);
-    } else {
-      console.log("Unknown password");
-    }
+  if (newPasswordValue) {
+    await setPassword(passwordName, newPasswordValue);
+    console.log(`Password ${passwordName} set 🎉`);
+  } else {
+    const newPasswordValue = await getPassword(passwordName);
+    console.log(`Ypour password is ${newPasswordValue} 🎉`);
   }
 }
 
-validateAccess();
+run();
